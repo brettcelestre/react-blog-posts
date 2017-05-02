55374fdf43a59a8349b40f44adadd9561baa65d9
Brett Celestre <bcelestre@gmail.com>
2017-05-02 09:13:58 -0700

    Initial Commit

---
+++ src/components/posts_index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+
+class PostsIndex extends Component {
+
+  componentDidMount(){
+    console.log('mount test');
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return(
+      <div>
+        Posts Index
+      </div>
+    )
+  };
+
+}
+
+export default connect(null, { fetchPosts })(PostsIndex);
