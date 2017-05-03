34b58428a47f90ebe49069495918976aa687737d
Brett Celestre <bcelestre@gmail.com>
2017-05-02 17:07:39 -0700

    Adds PostsNew component and sets up route

---
+++ src/components/posts_new.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+
+class PostsNew extends Component {
+  render(){
+    return (
+      <div>
+        PostsNew!
+      </div>
+    );
+  }
+}
+
+export default PostsNew;
