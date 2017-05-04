0b6090ba1a37d8e08503fb42fa8cd556104e4ae5
Brett Celestre <bcelestre@gmail.com>
2017-05-03 17:24:31 -0700

    Adds form validation to PostsNew

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,32 +1,46 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
 
   renderField(field, type){
+
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
           type="text"
           className="form-control"
           {...field.input}
         />
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
     );
   }
 
+  onSubmit(values){
+    console.log('onSubmit Values',values)
+  }
+
   render(){
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           name="title"
           label="Title"
           component={this.renderField}
         />
         <Field
-          name="tags"
-          label="Tags"
+          name="categories"
+          label="Categories"
           component={this.renderField}
         />
         <Field
@@ -34,11 +48,30 @@ class PostsNew extends Component {
           label="Post Content"
           component={this.renderField}
         />
+        <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
 }
 
+// Validates Field values
+function validate(values){
+  const errors = {};
+  // Validate the inputs from 'values'
+  if ( !values.title ) {
+    errors.title = "Enter a title!";
+  } else if ( !values.categories ) {
+    errors.categories = "Enter at least one category!";
+  } else if ( !values.content ) {
+    errors.content = "Enter some content!";
+  }
+
+  // If errors is empty, the form is fine to submit
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew);
